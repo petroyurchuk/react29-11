1993e74be70da6b7c49e6f752575ff4e30854aec
PetroYurchuk <petroyurchuk66@gmail.com>
2023-02-24 20:49:08 +0200

    add container

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const title = React.createElement(
-    'h1',
-    {
-        id: 'title',
-        className: 'red',
-    },
-    'Hello React'
-)
+// const title = React.createElement(
+//     'h1',
+//     {
+//         id: 'title',
+//         className: 'red',
+//     },
+//     "test"
+// )
 
+///With JSX
+const title = <h1 className="red">Test</h1>
+const list = (
+    <ul>
+        <li>List item 1</li>
+        <li>List item 2</li>
+        <li>List item 3</li>
+    </ul>
+)
+const content = (
+    <div>
+        {title}
+        {list}
+    </div>
+)
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{title}</React.StrictMode>)
+root.render(<React.StrictMode>{content}</React.StrictMode>)
