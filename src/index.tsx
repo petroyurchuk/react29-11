bafeaf88bddd4038d72f1397f843db43baea121f
PetroYurchuk <petroyurchuk66@gmail.com>
2023-02-27 18:56:53 +0200

    add App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-// const title = React.createElement(
-//     'h1',
-//     {
-//         id: 'title',
-//         className: 'red',
-//     },
-//     "test"
-// )
-
-///With JSX
-const title = <h1 className="red">Test</h1>
-const list = (
-    <ul>
-        <li>List item 1</li>
-        <li>List item 2</li>
-        <li>List item 3</li>
-    </ul>
-)
-const content = (
-    <div>
-        {title}
-        {list}
-    </div>
-)
+function App() {
+    return <h1>Hello App component</h1>
+}
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
