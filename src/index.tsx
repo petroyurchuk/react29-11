bc36d3d3542ec32fee7ee61c63b2348a0562ca9a
PetroYurchuk <petroyurchuk66@gmail.com>
2023-02-27 19:34:36 +0200

    add react fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-const App = () => {
+const Title = () => {
     return <h1>Hello App component</h1>
+}
+const Content = () => {
+    return (
+        <>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+                sunt dolorem eum nesciunt, iusto, doloremque eius quidem,
+                similique cupiditate cum esse excepturi laudantium labore quae
+                fugit dolores vitae maxime eaque!
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+                sed incidunt alias quasi dolore! Eos error officiis corporis
+                obcaecati quo? Illum nisi laudantium aliquid, totam animi harum
+                dolore atque neque?
+            </p>
+        </>
+    )
+}
+const App = () => {
+    return (
+        <>
+            <Title />
+            <Content />
+        </>
+    )
 }
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
