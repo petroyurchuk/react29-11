d849dc4c864d3cbccc0ac7f65371c24b0a1cd4d7
PetroYurchuk <petroyurchuk66@gmail.com>
2023-02-27 19:01:06 +0200

    add App component by arrow function

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function App() {
+const App = () => {
     return <h1>Hello App component</h1>
 }
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
