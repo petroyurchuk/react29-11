e99f5e38185196d7851bea456ceb7ce61001873c
PetroYurchuk <petroyurchuk66@gmail.com>
2023-03-03 19:43:23 +0200

    add css component

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,15 +1,17 @@
 import Header from 'container/Header/Header'
 import Main from 'container/Main/Main'
 import Footer from 'container/Footer/Footer'
+import CssBaseline from '@mui/material/CssBaseline'
 
 type Props = {}
 const App = (props: Props) => {
     return (
-        <div>
+        <>
+            <CssBaseline />
             <Header />
             <Main />
             <Footer />
-        </div>
+        </>
     )
 }
 export default App
