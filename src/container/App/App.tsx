f169386836eddebf564f51170abdf74ab1b1594b
PetroYurchuk <petroyurchuk66@gmail.com>
2023-03-03 19:22:50 +0200

    add components: Header,Main,Footer

---
+++ src/container/App/App.tsx
@@ -0,0 +1,15 @@
+import Header from '../Header/Header'
+import Main from '../Main/Main'
+import Footer from '../Footer/Footer'
+
+type Props = {}
+const App = (props: Props) => {
+    return (
+        <div>
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    )
+}
+export default App
