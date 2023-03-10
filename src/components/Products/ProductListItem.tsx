f25e77acb2e3fc8009690e9f38bd3a190fd87a47
PetroYurchuk <petroyurchuk66@gmail.com>
2023-03-10 20:39:16 +0200

    add state and functions decrement increment which call on click the buttons

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -15,7 +15,28 @@ type Props = {
     price: number
     image: string
 }
-class ProductListItem extends Component<Props> {
+type State = {
+    count: number
+}
+class ProductListItem extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            count: 1,
+        }
+        this.onIncrementClick = this.onIncrementClick.bind(this)
+        this.onDecrementClick = this.onDecrementClick.bind(this)
+    }
+    onDecrementClick() {
+        this.setState((prevState) => ({
+            count: prevState.count - 1,
+        }))
+    }
+    onIncrementClick() {
+        this.setState((prevState) => ({
+            count: prevState.count + 1,
+        }))
+    }
     render() {
         const { title, description, type, capacity, price, image } = this.props
         return (
@@ -34,9 +55,19 @@ class ProductListItem extends Component<Props> {
                         Price: <strong>${price}</strong>
                     </div>
                     <div className="product-quantity">
-                        <Button variant="outlined">-</Button>
-                        <TextField size="small" value="1" />
-                        <Button variant="outlined">+</Button>
+                        <Button
+                            variant="outlined"
+                            onClick={this.onDecrementClick}
+                        >
+                            -
+                        </Button>
+                        <TextField size="small" value={this.state.count} />
+                        <Button
+                            variant="outlined"
+                            onClick={this.onIncrementClick}
+                        >
+                            +
+                        </Button>
                     </div>
                     <CardActions className="btns-wrap">
                         <Button variant="outlined">Add to cart</Button>
@@ -46,37 +77,5 @@ class ProductListItem extends Component<Props> {
         )
     }
 }
-// const ProductListItem = ({
-//     title,
-//     description,
-//     type,
-//     capacity,
-//     price,
-//     image,
-// }: Props) => {
-//     return (
-//         <Card variant="outlined" className="product">
-//             <CardContent>
-//                 <div className="product-image">
-//                     <img src={image} alt={title} />
-//                 </div>
-//                 <h4 className="product-title">{title}</h4>
-//                 <div className="product-description">{description}</div>
-//                 <div className="product-features">Type: {type}</div>
-//                 <div className="product-features">Capacity: {capacity}GB</div>
-//                 <div className="product-price">
-//                     Price: <strong>${price}</strong>
-//                 </div>
-//                 <div className="product-quantity">
-//                     <Button variant="outlined">-</Button>
-//                     <TextField size="small" value="1" />
-//                     <Button variant="outlined">+</Button>
-//                 </div>
-//                 <CardActions className="btns-wrap">
-//                     <Button variant="outlined">Add to cart</Button>
-//                 </CardActions>
-//             </CardContent>
-//         </Card>
-//     )
-// }
+
 export default ProductListItem
