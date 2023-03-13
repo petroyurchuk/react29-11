6aae42544f230ab170231c31ae08a599637d69e2
PetroYurchuk <petroyurchuk66@gmail.com>
2023-03-13 20:02:29 +0200

    add functions on buttons

--- src/components/Products/ProductListItem.tsx
+++ src/components/Products/ProductListItem.tsx
@@ -5,6 +5,7 @@ import {
     CardContent,
     TextField,
 } from '@mui/material'
+import { useState } from 'react'
 
 import './ProductListItem.scss'
 type Props = {
@@ -23,6 +24,14 @@ const ProductListItem = ({
     price,
     image,
 }: Props) => {
+    const [count, setCount] = useState<number>(1)
+
+    const onIncrement = () => {
+        setCount((prevState) => prevState + 1)
+    }
+    const onDecrement = () => {
+        setCount((prevState) => prevState - 1)
+    }
     return (
         <Card variant="outlined" className="product">
             <CardContent>
@@ -37,9 +46,17 @@ const ProductListItem = ({
                     Price: <strong>${price}</strong>
                 </div>
                 <div className="product-quantity">
-                    <Button variant="outlined">-</Button>
-                    <TextField size="small" value="1" />
-                    <Button variant="outlined">+</Button>
+                    <Button
+                        variant="outlined"
+                        onClick={() => onDecrement()}
+                        disabled={count <= 1}
+                    >
+                        -
+                    </Button>
+                    <TextField size="small" value={count} />
+                    <Button variant="outlined" onClick={() => onIncrement()}>
+                        +
+                    </Button>
                 </div>
                 <CardActions className="btns-wrap">
                     <Button variant="outlined">Add to cart</Button>
