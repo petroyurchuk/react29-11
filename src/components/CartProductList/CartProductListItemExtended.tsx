3ff4628b0efa2083e5ec5b1f924f04c0dcb70c2b
PetroYurchuk <petroyurchuk66@gmail.com>
2023-03-24 20:02:38 +0200

    add Quantity component

--- src/components/CartProductList/CartProductListItemExtended.tsx
+++ src/components/CartProductList/CartProductListItemExtended.tsx
@@ -1,15 +1,20 @@
 import { Button, Card, CardContent, Grid } from '@mui/material'
 import { Product } from 'utils/productsArray'
 import DeleteIcon from '@mui/icons-material/Delete'
+import Quantity from 'components/Quantity/Quantity'
 type Props = {
     product: Product
     productCount: number
     removeProductFromCart: (id: number) => void
+    onDecrement: () => void
+    onIncrement: () => void
 }
 const CartProductListItemExtended = ({
     product,
     productCount,
     removeProductFromCart,
+    onDecrement,
+    onIncrement,
 }: Props) => {
     return (
         <Grid item xs={12} sm={4}>
@@ -21,6 +26,11 @@ const CartProductListItemExtended = ({
                     <h4>{product.title}</h4>
                     <p>Price for one item: $ {product.price}</p>
                     <p>Count: {productCount}</p>
+                    <Quantity
+                        count={productCount}
+                        onDecrement={() => console.log('test1')}
+                        onIncrement={() => console.log('test2')}
+                    />
                     <Button
                         onClick={() => removeProductFromCart(product.id)}
                         variant="outlined"
